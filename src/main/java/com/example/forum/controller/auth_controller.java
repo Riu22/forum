567e2243b01e8e40e329271d2115626a0e26c17b
Riu <riutordjaume@gmail.com>
2026-02-11 19:39:37 +0100

    initialize Vue frontend with basic structure and configuration; add essential components and setup for routing

--- src/main/java/com/example/forum/controller/auth_controller.java
+++ src/main/java/com/example/forum/controller/auth_controller.java
@@ -14,6 +14,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/auth")
 public class auth_controller {
